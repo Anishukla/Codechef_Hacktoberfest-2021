31292d890699302b0bea33f136e63ec5252c5fc8
singhsama <yashshanker1326@gmail.com>
2019-10-17 02:34:00 +0530

    Solution added in OCT19B

---
+++ OCT19B/MSV.py
@@ -0,0 +1,25 @@
+"""
+https://www.codechef.com/OCT19B/problems/MSV
+"""
+
+T = int(input())
+for i in range(T):
+    N = int(input())
+    L = list(map(int, input().split()))
+    L2 = [0]*N
+    res = 0
+    n = N-1
+    while n!=0:
+        if L2[n]==1:
+            n = n-1
+        req = 0
+        a = L[n]
+        for i in range(n-1, -1, -1):
+            if L[i]%L[n]==0:
+                L2[i] = 1
+                req = req+1
+       
+        res = max(req, res)
+        n = n-1
+   
+    print(res)
