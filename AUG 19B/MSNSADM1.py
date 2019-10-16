3c9b3da0af477fc8327f9ba94e555702f994d76b
ankit9437 <ygupta2008@yahoo.co.in>
2019-10-16 23:32:35 +0530

    Added codes from August long challenge.

---
+++ AUG 19B/MSNSADM1.py
@@ -0,0 +1,40 @@
+"""
+https://www.codechef.com/AUG19B/problems/MSNSADM1
+"""
+
+for i in range(int(input())):
+    r,c=map(int, input().split())
+    l1=[]
+    l2=[]
+    co=0
+    for j in range(r*c):
+        l1.append(j)
+    j=0
+    k=1
+    while(j<=r*c):
+        l2.append(j)
+        j+=c
+        if(j>=r*c and k<c):
+            j=k
+            k+=1
+    k=0
+    while(k<r*c):
+        l3=[]
+        l4=[]
+        l3=l1.copy()
+        l4=l2.copy()
+        n=0
+        while(n<r*c):
+           l3.remove(l1[n])
+           l4.remove(l2[n])
+           n+=k+1
+        k+=1
+        for x in range(r*c):
+            if(x not in l3 or x not in l4):
+                co+=1
+        print(co,end=" ")
+        co=0
+        
+        
+
+        
