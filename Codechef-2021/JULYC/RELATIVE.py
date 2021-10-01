4db37930a296c9a94270d8919c8daedd3c435270
Madhura <mayur.kongutte2002@gmail.com>
2021-10-01 08:57:11 +0530

    added solutions to problems EITA and RELATIVE

---
+++ Codechef-2021/JULYC/RELATIVE.py
@@ -0,0 +1,10 @@
+# https://www.codechef.com/JULY21C/problems/RELATIVE
+# https://www.codechef.com/users/mayur2506
+
+
+for _ in range(int(input())):
+    g,c=map(int,input().split())
+    print((c**2)//(2*g))
+
+
+    
