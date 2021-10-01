419bb6b18a6f01b7cdbc5ebaef973aba1d43c8b6
UnnatKr <79165109+UnnatKr@users.noreply.github.com>
2021-10-01 11:37:25 +0530

    Add files via upload

---
+++ Codechef-2021/JULY21C/Relativity.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+#define lln long long int 
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        lln g,c,h;
+        cin>>g>>c;
+        h=(c*c)/(2*g);
+        cout<<h<<endl;
+    }
+    return 0;
+}
